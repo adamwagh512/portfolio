d57a53f8a3983639b5b547f8cc2030a9e3d965d1
Adam Wagh <adamwagh@gmail.com>
2023-03-18 17:04:13 -0500

    t

--- portfolio/components/navbar1.jsx
+++ portfolio/components/navbar1.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsPersonLinesFill } from "react-icons/bs";
-import AdamWagh_logo from '../public/assets/AdamWagh_logo.jpg'
+import logo from '../public/assets/AdamWagh_logo.jpg'
 
 // Many individual styling comments were left out because I can automatically see them. To ensure that you can see them as well please make sure you are using
 // The Tailwind CSS IntelliSense extention in your IDE.
@@ -23,7 +23,7 @@ const navbar = () => {
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16 bg-gray-800">
         {/* Imports image and controls sizing */}
         <Image
-          src={AdamWagh_logo}
+          src={logo}
           alt="logo"
           width="125"
           height="50"
